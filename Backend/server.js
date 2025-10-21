733bdc4c6ffc3c727755a39893d7fd78f4f94e09
Akanksha Yadav <rd.akanksha05@gmail.com>
2025-10-21 19:36:19 +0530

    final

--- Backend/server.js
+++ Backend/server.js
@@ -90,7 +90,7 @@ app.get("/api/test", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 // ===== MongoDB Connection & Start Server =====
-mongoose.connect("mongodb://127.0.0.1:27017/formDB")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => console.log("Server running on port 5000"));
